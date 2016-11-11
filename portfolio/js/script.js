b067304566d0d36fa0639195b16442f29b7aca82
rahaduzzaman <rahad1@ymail.com>
2016-11-11 16:13:56 +0600

    protfolio Update

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,5 +1,9 @@
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
+	
+	new WOW().init();
+	
+	
 	var MQL = 1170;
 
 	//primary navigation slide-in effect
@@ -26,20 +30,31 @@ jQuery(document).ready(function($) {
 			this.previousTop = currentTop;
 		});
 	}
-
+	// This is for parallax slider effect
+	(function($) {
+		// Init Skrollr
+		var s = skrollr.init({
+			render : function(data) {
+				//Debugging - Log the current scroll position.
+				//console.log(data.curTop);
+			}
+		});
+	} )(jQuery);
+	
+	// This is for circle Progress
 	$('.circle1').circleProgress({
 		size : 150,
 		fill : {
 			gradient : ["black"]
 		}
 	});
-	
+
 	// Client Speech slider carousel
 	var cls = $('.owl-carousel');
 	cls.owlCarousel({
 		loop : true,
 		nav : false,
-		dots: true,
+		dots : true,
 		autoHeight : true,
 		responsive : {
 			0 : {
@@ -57,5 +72,44 @@ jQuery(document).ready(function($) {
 		}
 	});
 
+	//Animated Number
+	//Animated Number
+	$.fn.animateNumbers = function(stop, commas, duration, ease) {
+		return this.each(function() {
+			var $this = $(this);
+			var start = parseInt($this.text().replace(/,/g, ""));
+			commas = (commas === undefined) ? true : commas;
+			$({
+				value : start
+			}).animate({
+				value : stop
+			}, {
+				duration : duration == undefined ? 1000 : duration,
+				easing : ease == undefined ? "swing" : ease,
+				step : function() {
+					$this.text(Math.floor(this.value));
+					if (commas) {
+						$this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+					}
+				},
+				complete : function() {
+					if (parseInt($this.text()) !== stop) {
+						$this.text(stop);
+						if (commas) {
+							$this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+						}
+					}
+				}
+			});
+		});
+	};
+
+	$('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		var $this = $(this);
+		if (visible) {
+			$this.animateNumbers($this.data('digit'), false, $this.data('duration'));
+			$this.unbind('inview');
+		}
+	});
 
 });
