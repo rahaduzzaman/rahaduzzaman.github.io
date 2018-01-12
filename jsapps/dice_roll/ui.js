e6c1be65e63bea509acb824ec06a43641d4f68be
Md Rahaduzzaman <mdrahad.d9@gmail.com>
2018-01-13 01:50:05 +0600

    dice roll initial commit

---
+++ jsapps/dice_roll/ui.js
@@ -0,0 +1,14 @@
+// function to print the value
+function printValue(number){
+	var placeholder = document.getElementById('placeholder');
+	placeholder.innerHTML = number;
+}
+
+// Catch the element on which the event will trigger 
+var button = document.getElementById("button");
+
+// Create function to do the task when event triggered
+button.onclick = function() {
+  var result = dice.roll();
+  printValue(result);
+};
