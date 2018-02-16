f8b32ec5f3f1d32ee5983babda16d97114557643
Md Rahaduzzaman <mdrahad.d9@gmail.com>
2018-02-17 00:11:39 +0600

    bookmarker initial complete

--- jsapps/bookmarker/js/main.js
+++ jsapps/bookmarker/js/main.js
@@ -1,30 +1,25 @@
+var bform = document.getElementById('myForm');
 // Listen for form submit
-document.getElementById('myForm').addEventListener('submit', saveBookmark);
-
+bform.addEventListener('submit', saveBookmark)
 // Save Bookmark
 function saveBookmark(e) {
-    // Get form Values
+    // Get the Values of user input
     var siteName =  document.getElementById('siteName').value;
-    var sitUrl =  document.getElementById('siteUrl').value;
+    var siteUrl =  document.getElementById('siteUrl').value;
+
+    if (!validateForm(siteName, siteUrl)) {
+        return false;
+    }
+
 
     var bookmark = {
         name : siteName,
-        url : sitUrl
+        url : siteUrl
     };
-
-    /*
-    // Local Storage Test
-    localStorage.setItem('test', 'Hello World');
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
-    */
 
     // Test if Bookmark is Null
     if (localStorage.getItem('bookmarks') === null) {
-        // Init Array
         var bookmarks = [];
-        // Add to array
         bookmarks.push(bookmark);
         // Set to LocalStorage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
@@ -33,18 +28,72 @@ function saveBookmark(e) {
         var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
         // Add new bookmark to Bookmarks array
         bookmarks.push(bookmark);
-        // Re-set back to local Storage
+        // Reset back to local Storage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
     // Show the bookmarks
     fetchBookmarks();
 
+    bform.reset();
     // Prevent form from submitting
     e.preventDefault();
 }
 
-// Function to fetch the bookmarks
+function deleteBookmark(url) {
+    // Get bookmarks from local storage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    for (var i = 0; i < bookmarks.length; i++) {
+        if(url == bookmarks[i].url) {
+            bookmarks.splice(i, 1);
+        }
+    }
+
+    // Updated bookmarks again Set to LocalStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    // re run the show bookmarks 
+    fetchBookmarks();
+}
+
+
+// Function to render the bookmarks
 function fetchBookmarks() {
-    // console.log(bookmark);
+    // Get bookmarks from local storage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // get the bookmark holder ID
+    var bookmarkResults = document.getElementById('bookmarkResults');
+
+    bookmarkResults.innerHTML = '';
+    // Loop through the bookmarks
+    for(var i=0; i< bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarkResults.innerHTML += '<div class="single-bookmark well">' +
+                                        '<h3> Name: <span class="text-primary">'+ name + '</span></h3>' +
+                                        '<hr />' +
+                                        '<h4>' +
+                                        '<a class="btn btn-primary" target="_blank" href="'+url+'">Visit</a> ' +
+                                        '<button onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" type="submit">Delete</a> ' +
+                                        '</h4>' +
+                                    '</div>';
+    }
+}
+
+// Validate form
+function validateForm(sName, sUrl) {
+    if (!sName || !sUrl) {
+        alert('Please fill the form');
+    }
+
+    var exp = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(exp)
+
+    if (!sUrl.match(regex)) {
+        alert('Please use a Valid URL');
+        return false;
+    }
+    return true;
 }
